8e214e81ae995b52f175496586d81ae6f8a9c3c3
ShahriarImon <imon.shahriar012@gmail.com>
2022-06-21 19:49:26 +0600

    React core concept all doubt clear

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [photo, setPhoto] = useState([]);
+
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(res => res.json())
+      .then(data => setPhoto(data.slice(0, 20)))
+    console.log(photo)
+  }, [])
+
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          photo.map(element => <Component title={element.title} url={element.url}></Component>)
+        }
       </header>
     </div>
   );
 }
 
+const Component = (props) => {
+  const [count, setCount] = useState(0);
+  const styleCom = {
+    border: '5px solid buildQueries',
+    margin: '20px',
+    padding: '30px',
+    color: 'black',
+    backgroundColor: 'orange'
+  }
+  const countPLus=()=>{setCount(count+1)}
+  return (
+    <div style={styleCom}>
+      <img onClick={countPLus} src={props.url} alt="" />
+      <figcaption>
+        {props.title}
+      </figcaption>
+      <p>{count} times clicked</p>
+      <AnotherCom name={props.title} count={count} setCount={setCount}></AnotherCom>
+
+    </div>)
+}
+
+const AnotherCom = (props) => {
+  const aStyleCom = {
+    border: '5px solid red',
+    margin: '10px',
+    padding: '10px',
+    color: 'grey',
+    backgroundColor: 'yellow'
+  }
+  const reduceCount =()=>{
+    props.setCount(props.count-1)
+  }
+  return (
+    <div onClick={reduceCount} style={aStyleCom}>
+      <p>Another one: {props.name}</p>
+      <p>{props.count} times clicked</p>
+
+    </div>)
+
+}
 export default App;
